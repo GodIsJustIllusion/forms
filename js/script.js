e9ed3502130840cbd820afad6575e2875c0597e0
Igor_A <87186964+GodIsJustIllusion@users.noreply.github.com>
2022-07-27 09:04:34 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
-let enterForm = document.forms.enterForm;
-let fff = enterForm.email;
-let ddd = enterForm.pass;
-
 let modal = document.getElementById("my_modal");
 let btn = document.getElementById("btn_modal_window");
 let span = document.getElementsByClassName("close_modal_window")[0];
 
-function validate() {
-    if (fff.value == "" && ddd.value == "") {
-        alert ("Укажите ваше имя") } else {
+let enterForm = document.forms.enterForm;
+if (enterForm) {
+    let fff = enterForm.email;
+    let ddd = enterForm.pass;
+
+    enterForm.addEventListener('submit', function (event) {
+        event.preventDefault()
+    })
+
+    btn.onclick = function() {
+        if (fff.checkValidity() && ddd.checkValidity()) {
             modal.style.display = "block";
-                
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-            
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
+            document.querySelectorAll('input').forEach(el=>el.value = '');
         }
+    }
+}
+
+let regForm = document.forms.regForm;
+if (regForm) {
+    let nameReg = regForm.name;
+    let emailReg = regForm.email;
+    let passReg = regForm.pass;
+    let passrepeatReg = regForm.passrepeat;
+
+    regForm.addEventListener('submit', function (event) {
+        event.preventDefault()
+    })
+
+    btn.onclick = function() {
+        if (nameReg.checkValidity() && emailReg.checkValidity() && passReg.checkValidity() && passrepeatReg.checkValidity() && passReg.value == passrepeatReg.value) {
+            modal.style.display = "block";
+            document.querySelectorAll('input').forEach(el=>el.value = '');
+        }
+    }
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
